5bb8985685ec21cc5cbbbca51b8598419f2c7f60
Reyn <jl88744653@gmail.com>
2024-10-01 23:23:15 +0800

    Update window.ts

--- extensions/core/source/app/layer/window.ts
+++ extensions/core/source/app/layer/window.ts
@@ -11,9 +11,7 @@ abstract class UIWindow extends UILayer {
     public load() {
         if ( this._view ) return;
 
-        if(this.path.startsWith("http://") || this.path.startsWith("https://")) {
-            this.load_remote(path);
-        } else if(this.path)
+        // TODO::加载预制体
     }
 
     private load_remote(path: string) {
